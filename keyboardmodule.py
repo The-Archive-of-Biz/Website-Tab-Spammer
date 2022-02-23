e8044af052f895fc5c1f7ff039736e3648863d27
BizCoCoding <66413191+BizCoCoding@users.noreply.github.com>
2022-02-23 18:10:26 +0000

    Create keyboardmodule.py

---
+++ keyboardmodule.py
@@ -0,0 +1,9 @@
+import keyboard
+import time
+time.sleep(8)
+for x in range (6000):
+    keyboard.press_and_release('ctrl + t')
+    time.sleep(0.5)
+    keyboard.press_and_release('ctrl + t')
+    time.sleep(0.5)
+    keyboard.press_and_release('ctrl + t')
