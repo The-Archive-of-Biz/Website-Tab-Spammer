aa42e87b5587abda7dd838558904da87b1708707
BizCoCoding <66413191+BizCoCoding@users.noreply.github.com>
2022-02-23 18:15:11 +0000

    Update webbrowser.py

--- webbrowser.py
+++ webbrowser.py
@@ -1,6 +1,7 @@
 import webbrowser
 import time
-website = input("What URL should I use?")
+website = input('''What URL should I use?
+''')
 time.sleep(8)
 while True:
     webbrowser.open_new(website)
